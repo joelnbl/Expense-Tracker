71e2483aa2175b099100120e997154aae9ac8956
Joel Bello <joel8bl@gmail.com>
2021-06-28 03:06:54 -0400

    rename props with the specific function

---
+++ src/components/Expenses.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+const ExpenseDate = (props) => {
+  const { items } = props;
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={items[0].title}
+        amount={items[0].amount}
+        date={items[0].date}
+      />
+      <ExpenseItem
+        title={items[1].title}
+        amount={items[1].amount}
+        date={items[1].date}
+      />
+      <ExpenseItem
+        title={items[2].title}
+        amount={items[2].amount}
+        date={items[2].date}
+      />
+    </div>
+  );
+};
+
+export default ExpenseDate;
