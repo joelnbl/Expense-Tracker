8a26d7c2acf3282e5f84a7c57e55bfec247bd93a
Joel Bello <joel8bl@gmail.com>
2021-06-28 02:51:51 -0400

    Splitting Components Into Multiple Components

--- src/App.js
+++ src/App.js
@@ -27,17 +27,17 @@ const App = () => {
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
-        date={expenses[0].date.toDateString()}
+        date={expenses[0].date}
       />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
-        date={expenses[1].date.toDateString()}
+        date={expenses[1].date}
       />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
-        date={expenses[2].date.toDateString()}
+        date={expenses[2].date}
       />
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  console.log(props);
   const { title, amount, date } = props;
 
   return (
     <div className="expense-item">
-      <div>{date}</div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
