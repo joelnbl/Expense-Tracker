53dadc620ee01856df2a3a3dba3b9daac980a865
Joel Bello <joel8bl@gmail.com>
2021-06-28 21:26:52 -0400

    Organizing Component Files

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
   const { title, amount, date } = props;
