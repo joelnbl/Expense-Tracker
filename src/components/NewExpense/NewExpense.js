e4bcb33ae26ea3fd9d0a9518557591fd8dfdb1f3
Joel Bello <joel8bl@gmail.com>
2021-07-02 18:54:59 -0400

    styles fixed

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import ExpenseForm from "./ExpenseForm";
-import "./NewExpense.css";
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
 
 const NewExpense = (props) => {
   const [isEditing, setIsEditing] = useState(false);
@@ -18,19 +18,20 @@ const NewExpense = (props) => {
   const startEditingHandler = () => {
     setIsEditing(true);
   };
+
   const stopEditingHandler = () => {
     setIsEditing(false);
   };
 
   return (
-    <div className="new-expense">
+    <div className='new-expense'>
       {!isEditing && (
         <button onClick={startEditingHandler}>Add New Expense</button>
       )}
       {isEditing && (
         <ExpenseForm
-          onCancel={stopEditingHandler}
           onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
         />
       )}
     </div>
