d9181fa5b6cd71e0c0797d6c7e5910c898a8a8b4
Joel Bello <joel8bl@gmail.com>
2021-07-02 11:36:50 -0400

    Rendering Lists of Data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,21 +17,13 @@ const ExpenseDate = (props) => {
           selected={filteredYear}
           onFilterChange={filterChangeHandler}
         />
-        <ExpenseItem
-          title={items[0].title}
-          amount={items[0].amount}
-          date={items[0].date}
-        />
-        <ExpenseItem
-          title={items[1].title}
-          amount={items[1].amount}
-          date={items[1].date}
-        />
-        <ExpenseItem
-          title={items[2].title}
-          amount={items[2].amount}
-          date={items[2].date}
-        />
+        {items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
