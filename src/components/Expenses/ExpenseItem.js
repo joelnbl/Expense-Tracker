4a626dedb7df95ca2a54ef6c279be9e1c2044866
Joel Bello <joel8bl@gmail.com>
2021-07-01 22:15:43 -0400

    Adding Form Inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -13,10 +13,10 @@ const ExpenseItem = (props) => {
 
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{titleChanger}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">${amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
